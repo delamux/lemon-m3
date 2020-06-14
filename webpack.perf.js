dee8a5be0ebae52b82fee735b862b86046066df5
Luis del Amo <luis@salonkee.lu>
2020-06-15 01:34:39 +0200

    webpack performance

---
+++ webpack.perf.js
@@ -0,0 +1,8 @@
+const merge = require("webpack-merge");
+const prod = require("./webpack.prod");
+
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+
+module.exports = merge(prod, {
+  plugins: [new BundleAnalyzerPlugin()],
+});
